5ed8530d6f3e92b18de437d8affad6fd27d05711
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-17 22:15:47 +0530

    feat: added wishlist page

--- src/pages/wishList/WishList.jsx
+++ src/pages/wishList/WishList.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { topPicks } from "../../backend/db/products";
+import { ProductCard } from "../../components";
+import "./wishlist.css"
 function WishList() {
   return (
-    <div>WishList Page</div>
-  )
+    <>
+      <h1 className="text-center page-title">Wishlist</h1>
+    <div className="wishlist-container">
+      {topPicks.map((ele) => {
+        return (
+          <ProductCard
+            key={ele._id}
+            title={ele.title}
+            brand={ele.brand}
+            rating={ele.rating}
+            oldPrice={ele.oldPrice}
+            newPrice={ele.price}
+            imgSrc={ele.imgSrc}
+          />
+        );
+      })}
+      
+    </div>
+    </>
+  );
 }
 
-export default WishList
+export default WishList;
