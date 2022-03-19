c08c33b62b844f9f2d817623ef6417f370716ec5
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-19 21:28:34 +0530

    feat: added data to backend

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,25 +8,29 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Laptop",
+    title: "Laptop",
+    categoryName:"laptop",
     imgSrc: "/assests/categories/laptops.png",
     link: "/products/?category=laptop"
   },
   {
     _id: uuid(),
-    categoryName: "Tablet",
+    title: "Tablet",
+    categoryName:"tablet",
     imgSrc: "/assests/categories/tablets.jpeg",
     link: "/products/?category=tablet"
   },
   {
     _id: uuid(),
-    categoryName: "Phone",
+    title: "Phone",
+    categoryName:"phone",
     imgSrc: "/assests/categories/phones.jpg",
     link: "/products/?category=phone"
   },
   {
     _id: uuid(),
-    categoryName: "Smart Watch",
+    title: "Smart Watch",
+    categoryName:"smartWatch",
     imgSrc: "/assests/categories/smart_watches.jpg",
     link: "/products/?category=smart-watch"
   },
