3f0e3c6f1d39c40dfa1cb94b8e3220a4a28acb80
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-21 05:56:08 +0530

    feat: added protected routes

--- src/app-routes/AppRoutes.jsx
+++ src/app-routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Mockman from "mockman-js";
+import ProtectedRoutes from "./ProtectedRoutes";
 import {
   HomePage,
   Cart,
@@ -15,12 +16,15 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="/cart" element={<Cart />}></Route>
       <Route path="/products" element={<ProductListing />}></Route>
-      <Route path="/wishlist" element={<WishList />}></Route>
       <Route path="/signup" element={<SignUP />}></Route>
-      <Route path="/signout" element={<SignOut />}></Route>
       <Route path="/signin" element={<SignIn />}></Route>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/signout" element={<SignOut />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/wishlist" element={<WishList />}></Route>
+      </Route>
+
       <Route path="/mockman" element={<Mockman />} />
     </Routes>
   );
