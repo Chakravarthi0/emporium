39a96c5bf5920c35552fdf868242064437558d46
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-25 23:57:05 +0530

    feat: made signin, signup private for signedin user

--- src/app-routes/AppRoutes.jsx
+++ src/app-routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{useEffect} from "react";
+import {useAuth} from "../context/index"
 import Mockman from "mockman-js";
 import ProtectedRoutes from "./ProtectedRoutes";
 import {
@@ -10,15 +11,28 @@ import {
   SignOut,
   SignIn,
 } from "../pages/index";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function AppRoutes() {
+
+  const {
+    authState: { token },
+  } = useAuth();
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      {!token ? (
+        <>
+          <Route path="/signup" element={<SignUP />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+        </>
+      ) : (
+        <>
+          <Route path="/signup" element={<Navigate replace to="/" />}></Route>
+          <Route path="/signin" element={<Navigate replace to="/" />}></Route>
+        </>
+      )}
       <Route path="/products" element={<ProductListing />}></Route>
-      <Route path="/signup" element={<SignUP />}></Route>
-      <Route path="/signin" element={<SignIn />}></Route>
       <Route element={<ProtectedRoutes />}>
         <Route path="/signout" element={<SignOut />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
