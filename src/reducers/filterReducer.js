5f85c4f7860eb55ee164712967877dee4fdc93d3
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-26 00:04:37 +0530

    feat: added browse by category

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,44 +1,67 @@
 import { actionTypes } from "./actionTypes";
 
 const filterReducer = (state, action) => {
-
-  const {CHANGE_CATEGORIES, CLEAR_CATEGORIES, CHANGE_PRICE_RANGE,CHANGE_RATING, SORT_ASCENDING, SORT_DESCENDING, CLEAR_FILTERS, TOGGLE_INSTOCK } = actionTypes;
+  console.log("dispatch..", action)
+  const {
+    CHANGE_CATEGORIES,
+    CLEAR_CATEGORIES,
+    SET_SINGLE_CATEGORY,
+    CHANGE_PRICE_RANGE,
+    CHANGE_RATING,
+    SORT_ASCENDING,
+    SORT_DESCENDING,
+    CLEAR_FILTERS,
+    TOGGLE_INSTOCK,
+  } = actionTypes;
   switch (action.type) {
     case CHANGE_CATEGORIES:
-
       if (state.categories.includes(action.payload)) {
         const res = state.categories.slice();
         const index = state.categories.indexOf(action.payload);
         res.splice(index, 1);
-        return { 
-          ...state, categories: res 
+        return {
+          ...state,
+          categories: res,
         };
-      } 
-      else {
-        return { 
-          ...state, categories: [...state.categories, action.payload] 
+      } else {
+        return {
+          ...state,
+          categories: [...state.categories, action.payload],
         };
       }
     case CLEAR_CATEGORIES:
-      return { 
-        ...state, categories: [] 
+      return {
+        ...state,
+        categories: [],
+      };
+    case SET_SINGLE_CATEGORY:
+      return {
+        sortBy: "",
+        categories: [action.payload],
+        rating: 1,
+        includeOutOfStock: false,
+        maxPrice: 50000,
       };
     case CHANGE_PRICE_RANGE:
       return {
-         ...state, maxPrice: action.payload 
-        };
+        ...state,
+        maxPrice: action.payload,
+      };
     case CHANGE_RATING:
-      return { 
-        ...state, rating: action.payload
-       };
+      return {
+        ...state,
+        rating: action.payload,
+      };
     case SORT_ASCENDING:
-      return { 
-        ...state, sortBy: "ascending" 
+      return {
+        ...state,
+        sortBy: "ascending",
       };
     case SORT_DESCENDING:
-      return { 
-        ...state, sortBy: "descending"
-       };
+      return {
+        ...state,
+        sortBy: "descending",
+      };
     case CLEAR_FILTERS:
       return {
         sortBy: "",
@@ -49,8 +72,10 @@ const filterReducer = (state, action) => {
       };
     case TOGGLE_INSTOCK:
       return {
-         ...state, includeOutOfStock: !state.includeOutOfStock 
+        ...state,
+        includeOutOfStock: !state.includeOutOfStock,
       };
+
     default:
       return state;
   }
