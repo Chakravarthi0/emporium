ba9e7363725d0d77e8aadb112229d8cceabd637b
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-25 23:55:18 +0530

    feat: added scroll to top

---
+++ src/layouts/scroll-to-top/ScrollToTop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./scroll-to-top.css";
+
+function ScrollToTop() {
+  const scrollUp = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <div className="scroll-to-top-container">
+      <button
+        className="btn btn-primary btn-float btn-scroll-to-top"
+        onClick={scrollUp}
+      >
+        <i className="fas fa-arrow-up"></i>
+      </button>
+    </div>
+  );
+}
+
+export default ScrollToTop;
