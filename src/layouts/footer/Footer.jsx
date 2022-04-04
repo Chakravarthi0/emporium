c3eff1e0a4ce3d8e35f75f91b70acbe0ae09bd48
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-04-04 14:44:21 +0530

    fix: link fix

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -7,7 +7,11 @@ function Footer() {
       <p>Developed by Saran Chakravarthi</p>
       <ul className="social-links list">
         <li>
-          <a className="link" href="https://github.com/Chakravarthi0">
+          <a
+            className="link"
+            href="https://github.com/Chakravarthi0"
+            target="_blank"
+          >
             <i className="fab fa-github"></i>
           </a>
         </li>
@@ -15,12 +19,17 @@ function Footer() {
           <a
             className="link"
             href="https://www.linkedin.com/in/saran-chakravarthi"
+            target="_blank"
           >
             <i className="fab fa-linkedin"></i>
           </a>
         </li>
         <li>
-          <a className="link" href="https://twitter.com/SaranChakravar3">
+          <a
+            className="link"
+            href="https://twitter.com/SaranChakravar3"
+            target="_blank"
+          >
             <i className="fab fa-twitter"></i>
           </a>
         </li>
